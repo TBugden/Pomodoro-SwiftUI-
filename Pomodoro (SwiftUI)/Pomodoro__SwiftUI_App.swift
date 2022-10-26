37c1c5184cd54380e92fdad57d402c32be931327
Trevor <bugdent@gmail.com>
2022-10-26 16:02:33 -0700

    Initial Commit

---
+++ Pomodoro (SwiftUI)/Pomodoro__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro__SwiftUI_App.swift
+//  Pomodoro (SwiftUI)
+//
+//  Created by Jesse Chan on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
